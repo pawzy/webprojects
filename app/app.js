452d26c4d34fa3cad238d735fa6493111ae9251a
triinvask <triinvask@gmail.com>
2016-03-10 20:20:45 +0200

    Login test

--- app/app.js
+++ app/app.js
@@ -2,11 +2,13 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+]).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/login', {
+        templateUrl: 'modules/authentication/views/login.html',
+        controller: 'LoginController'
+    }).otherwise({redirectTo: '/view1'});
 }]);
