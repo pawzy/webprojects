60b293722839198cb3dac5fa3f68e39f290f865a
triinvask <triinvask@gmail.com>
2016-03-10 21:42:55 +0200

    http://jasonwatmore.com/post/2014/05/26/AngularJS-Basic-HTTP-Authentication-Example.aspx http login working

--- app/app.js
+++ app/app.js
@@ -1,14 +1,34 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
+// declare modules
+angular.module('Authentication', []);
+
+angular.module('BasicHttpAuthExample', [
     'ngRoute',
     'myApp.view1',
     'myApp.view2',
-    'myApp.version'
+    'myApp.version',
+    'Authentication',
+    'ngCookies'
 ]).config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/login', {
-        templateUrl: 'modules/authentication/views/login.html',
-        controller: 'LoginController'
-    }).otherwise({redirectTo: '/view1'});
-}]);
+        $routeProvider.when('/login', {
+            templateUrl: 'modules/authentication/views/login.html',
+            controller: 'LoginController'
+        }).otherwise({redirectTo: '/view1'});
+    }])
+
+    .run(['$rootScope', '$location', '$cookieStore', '$http',
+        function ($rootScope, $location, $cookieStore, $http) {
+            // keep user logged in after page refresh
+            $rootScope.globals = $cookieStore.get('globals') || {};
+            if ($rootScope.globals.currentUser) {
+                $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+            }
+
+            $rootScope.$on('$locationChangeStart', function (event, next, current) {
+                // redirect to login page if not logged in
+                if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                    $location.path('/login');
+                }
+            });
+        }]);
